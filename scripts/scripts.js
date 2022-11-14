030431aa59b07ba48acd5d70e1e90121e87e58fe
BrandonLott <brandon425@revature.net>
2022-11-14 16:50:48 -0500

    Calculator is fully functional, and working well, now on to mobile 1st design elements and a menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,30 +14,89 @@ class Calculator{
         this.operation = '';
     }
 
-    delete(){}
+    delete(){
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    }
 
     appendNumber(number){
-        
-        this.currentOperand = number;
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString();
         
     }
 
-    chooseOperation(operation){}
+    chooseOperation(operation){
+        if (this.currentOperand === '')return;
+        if (this.previousOperand !== ''){
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
 
-    compute(){}
+    compute(){
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
 
-    wasEqualsClicked(equalsWasClicked){}
-
-    updateDisplay(){
-        
-        this.currentOperandTextElement.innerText = this.currentOperand;
+            case '+':
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;
+            case '×':
+                computation = prev * current;
+                break;
+            case '÷':
+                computation = prev / current;
+                break;
+            default:
+                return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
        
     }
 
+    wasEqualsClicked(equalsWasClicked){
+        if (equalsWasClicked === true){
+            this.clear()
+        }
+    }
 
-
-
-
+    updateDisplay(){
+        this.currentOperandTextElement.innerText = 
+        this.getDisplayNumber(this.currentOperand)
+        if(this.operation != null){
+            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        }else{
+            this.previousOperandTextElement.innerText = ''
+        }  
+        //if current button clicked is '=' store evaluated expression in history   
+    }
+    getDisplayNumber(number){
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)){
+            integerDisplay = ''
+        }else{
+            integerDisplay = integerDigits.toLocaleString('en',{maximumFractionDigits: 0}) 
+        }
+        if(decimalDigits != null){
+            return `${integerDisplay}.${decimalDigits}`
+        }else{
+            return integerDisplay
+        }
+        //const floatNumber = parseFloat(number)
+        //if (isNaN(floatNumber)) return ''
+        //return floatNumber.toLocaleString('en');
+    }
 
 }
 
@@ -54,17 +113,45 @@ let equalsWasClicked = false;
 /*Calculator class bluprinted, time to use*/
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
-console.log(numberButtons);
+
 numberButtons.forEach(button => {
     
     button.addEventListener('click', () => {
         //check to see if the last buttin clicked was the  '=' if
         //it was then clear current contents and update with new information
-        
-        calculator.appendNumber(button.innerText)
-        calculator.updateDisplay()
+        calculator.wasEqualsClicked(equalsWasClicked);
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
         equalsWasClicked = false;
         
     })
 })
 
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        equalsWasClicked = false;
+        console.log('equalswasClicked set to : ', equalsWasClicked);
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    equalsWasClicked = true;
+    console.log('equalswasClicked set to : ', equalsWasClicked);
+    calculator.compute()
+    calculator.updateDisplay()
+    //i want to clear the value when a number button is clicked
+//testing
+    //if (equalsWasClicked)) {alert('you clicked equals'
+});
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+});
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+});
